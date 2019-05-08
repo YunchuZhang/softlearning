ced7f84ebbd3ab1ebe785a27d27cd9c7dde09b6a
Yash-5 <ychandar@andrew.cmu.edu>
2019-05-08 19:33:29 -0400

    sac + vae

---
+++ softlearning/algorithms/sac_vae.py
@@ -0,0 +1,21 @@
+from .sac import SAC
+
+class SACVAE(SAC):
+    def __init__(self, VAETrainer, train_vae_every=10, **kwargs):
+        super(SACVAE, self).__init__(**kwargs)
+        self.VAETrainer = VAETrainer
+        self.vae_num_epoch = 0
+        self.train_vae_every = train_vae_every
+
+    def _training_before_hook(self):
+        super()._training_before_hook()
+        # Should work other than the fact that vae trainer uses data['next_obs']
+        self.VAETrainer.train_epoch(self.vae_num_epoch, self.sampler.random_batch)
+        self.vae_num_epoch += 1
+
+    def _epoch_before_hook(self):
+        super()._epoch_before_hook()
+        if self._epoch and self._epoch % self.train_vae_every == 0:
+            # Should work other than the fact that vae trainer uses data['next_obs']
+            self.VAETrainer.train_epoch(self.vae_num_epoch, self.sampler.random_batch)
+            self.vae_num_epoch += 1
