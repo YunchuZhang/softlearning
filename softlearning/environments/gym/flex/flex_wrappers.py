e04be0a9849377d644a19cb642c9e0e686f88975
George Joseph <Shade5@users.noreply.github.com>
2019-05-15 20:04:48 -0400

    Multirobot

--- softlearning/environments/gym/flex/flex_wrappers.py
+++ softlearning/environments/gym/flex/flex_wrappers.py
@@ -7,53 +7,56 @@ from gym import spaces
 
 
 class FetchReach(gym.Env):
-        def __init__(self):
-                self.cfg = YamlConfig('/home/katadh/Documents/Research/VMGE/softlearning/softlearning/environments/gym/flex/fetch_cube.yaml')
-            # self.cfg = YamlConfig('/home/a/workspace/katerina/rlkit/examples/her/cfg/fetch_cube.yaml')
+	def __init__(self, render=False):
+		# self.cfg = YamlConfig('/home/katadh/Documents/Research/VMGE/softlearning/softlearning/environments/gym/flex/fetch_cube.yaml')
+		self.cfg = YamlConfig('/home/a/workspace/katerina/rlkit/examples/her/cfg/fetch_cube.yaml')
 
-                self.numAgents = self.cfg['scene']['NumAgents'] = 1
-                self.cfg['scene']['NumPerRow'] = np.sqrt(np.floor(self.numAgents))
-                self.cfg['scene']['SampleInitStates'] = True
-                self.cfg['scene']['InitialGrasp'] = False
-                self.cfg['scene']['RelativeTarget'] = False
-                self.cfg['scene']['DoDeltaPlanarControl'] = True
-                self.cfg['scene']['DoGripperControl'] = True
-                self.cfg['scene']['InitialGraspProbability'] = 1
-                self.cfg['scene']['DoWristRollControl'] = False
+		self.cfg['gym']['renderBackend'] = render
+		self.numAgents = self.cfg['scene']['NumAgents'] = 1
+		self.cfg['scene']['NumPerRow'] = np.sqrt(np.floor(self.numAgents))
+		self.cfg['scene']['SampleInitStates'] = True
+		self.cfg['scene']['InitialGrasp'] = False
+		self.cfg['scene']['RelativeTarget'] = False
+		self.cfg['scene']['DoDeltaPlanarControl'] = True
+		self.cfg['scene']['DoGripperControl'] = True
+		self.cfg['scene']['InitialGraspProbability'] = 1
+		self.cfg['scene']['DoWristRollControl'] = False
 
-                set_flex_bin_path('/home/katadh/Documents/Research/flex/FlexRobotics/bin')
-                # set_flex_bin_path('/home/a/workspace/FlexRobotics/bin')
-                self.env = FlexVecEnv(self.cfg)
+		# set_flex_bin_path('/home/katadh/Documents/Research/flex/FlexRobotics/bin')
+		set_flex_bin_path('/home/a/workspace/FlexRobotics/bin')
+		self.env = FlexVecEnv(self.cfg)
 
-                self.previous_observation = None
-                self.action_space = self.env.action_space
-                self.observation_space = spaces.Dict(
-                        {"achieved_goal": spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32),
-                         "desired_goal": spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32),
-                         "observation": spaces.Box(low=-np.inf, high=np.inf, shape=(8,), dtype=np.float32)})
+		self.previous_observation = None
+		self.action_space = self.env.action_space
+		self.observation_space = spaces.Dict(
+			{"achieved_goal": spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32),
+			 "desired_goal": spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32),
+			 "observation": spaces.Box(low=-np.inf, high=np.inf, shape=(8,), dtype=np.float32)})
 
-        def reset(self):
-                self.env.reset()
-                for i in range(20):
-                        obs, reward, done, info = self.env.step(np.array([0, 0, 0, -1]))
-                        self.previous_observation = obs[0][:4].copy()
+	def reset(self):
+		self.env.reset()
+		for i in range(20):
+			obs, reward, done, info = self.env.step(np.array([0, 0, 0, -1]))
+			self.previous_observation = obs[0][:4].copy()
 
-                return {'observation': np.append(obs[0][:4], np.zeros(4)), 'achieved_goal': obs[0][:3],
-                        'desired_goal': obs[0][4:8]}
+		return {'observation': np.append(obs[0][:4], np.zeros(4)), 'achieved_goal': obs[0][:3],
+		        'desired_goal': obs[0][4:8]}
 
-        def step(self, action):
-                obs, reward, done, info = self.env.step(action)
-                velocity = obs[0][:4] - self.previous_observation
-                self.previous_observation = obs[0][:4].copy()
-                return {'observation': np.append(obs[0][:4], velocity), 'achieved_goal': obs[0][:3],
-                        'desired_goal': obs[0][4:8]}, reward[0], done[0], {'is_success': reward[0] + 1}
+	def step(self, action):
+		obs, reward, done, info = self.env.step(action)
+		velocity = obs[0][:4] - self.previous_observation
+		self.previous_observation = obs[0][:4].copy()
+		return {'observation': np.append(obs[0][:4], velocity), 'achieved_goal': obs[0][:3],
+		        'desired_goal': obs[0][4:8]}, reward[0], done[0], {'is_success': reward[0] + 1}
 
-        def compute_reward(self, achieved_goal, desired_goal, info):
-                return self.env.compute_reward(achieved_goal[None, :], desired_goal[None, :], info)
-        
+	def compute_reward(self, achieved_goal, desired_goal, info):
+		return self.env.compute_reward(achieved_goal[None, :], desired_goal[None, :], info)
+
+
 class FetchPush(gym.Env):
 	def __init__(self):
-		self.cfg = YamlConfig('/home/katadh/Documents/Research/VMGE/softlearning/softlearning/environments/gym/flex/fetch_push.yaml')
+		self.cfg = YamlConfig(
+			'/home/katadh/Documents/Research/VMGE/softlearning/softlearning/environments/gym/flex/fetch_push.yaml')
 		# self.cfg = YamlConfig('/home/a/workspace/katerina/rlkit/examples/her/cfg/fetch_push.yaml')
 
 		self.numAgents = self.cfg['scene']['NumAgents'] = 1
@@ -95,3 +98,51 @@ class FetchPush(gym.Env):
 
 	def compute_reward(self, achieved_goal, desired_goal, info):
 		return self.env.compute_reward(achieved_goal[None, :], desired_goal[None, :], info)
+
+
+class FetchReachMultiRobot:
+	def __init__(self):
+		# self.cfg = YamlConfig('/home/georgejo/rlkit/examples/her/cfg/fetch_reach.yaml')
+		self.cfg = YamlConfig('/home/a/workspace/katerina/rlkit/examples/her/cfg/fetch_reach.yaml')
+
+		self.cfg['scene']['SampleInitStates'] = True
+		self.cfg['scene']['InitialGrasp'] = False
+		self.cfg['scene']['RelativeTarget'] = False
+		self.cfg['scene']['DoDeltaPlanarControl'] = True
+		self.cfg['scene']['DoGripperControl'] = True
+		self.cfg['scene']['InitialGraspProbability'] = 1
+		self.cfg['scene']['DoWristRollControl'] = False
+
+		# set_flex_bin_path('/home/georgejo/FlexRobotics/bin')
+		set_flex_bin_path('/home/a/workspace/FlexRobotics/bin')
+		self.env = FlexVecEnv(self.cfg)
+
+		self.previous_observation = None
+		self.action_space = self.env.action_space
+		self.observation_space = spaces.Dict(
+			{"achieved_goal": spaces.Box(low=-np.inf, high=np.inf, shape=(3, 1), dtype=np.float32),
+			 "desired_goal": spaces.Box(low=-np.inf, high=np.inf, shape=(3, 1), dtype=np.float32),
+			 "observation": spaces.Box(low=-np.inf, high=np.inf, shape=(8, 1), dtype=np.float32)})
+
+	def reset(self):
+		self.env.reset()
+		for i in range(20):
+			obs, reward, done, info = self.env.step(np.zeros((50, 4)))
+		self.previous_observation = obs[:, :4].copy()
+
+		return {'observation': np.append(obs[:, :4], np.zeros((50, 4)), axis=1), 'achieved_goal': obs[:, :3],
+		        'desired_goal': obs[:, 4:8]}
+
+	def step(self, action):
+		obs, reward, done, info = self.env.step(action)
+		velocity = obs[:, :4] - self.previous_observation
+		self.previous_observation = obs[:, :4].copy()
+		return {'observation': np.append(obs[:, :4], velocity, axis=1), 'achieved_goal': obs[:, :3],
+		        'desired_goal': obs[:, 4:8]}, reward, done, {'is_success': reward + 1}
+
+	def compute_reward(self, achieved_goal, desired_goal, info):
+		return self.env.compute_reward(achieved_goal[None, :], desired_goal[None, :], info)
+
+	@property
+	def unwrapped(self):
+		return self
