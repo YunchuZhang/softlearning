46129f283464e62a9c917ae391e2f4d5e6c8507b
katadh <katadh@sbcglobal.net>
2019-08-19 11:31:51 -0400

    fixed repeating batch for larger batch sizes

--- softlearning/samplers/multiagent_sampler.py
+++ softlearning/samplers/multiagent_sampler.py
@@ -73,7 +73,7 @@ class MultiAgentSampler(BaseSampler):
 
 
         if self.initialized and self.memory3D_sampler:
-            active_obs = [np.repeat(field, 2, 0) for field in active_obs]
+            active_obs = [np.vstack([field] * int(self._batch_size / self.num_agents)) for field in active_obs]
             active_obs = self.session.run(self.memory3D_sampler,feed_dict={self.obs_ph[0]:active_obs[0],self.obs_ph[1]:active_obs[1],self.obs_ph[2]:active_obs[2],\
                 self.obs_ph[3]:active_obs[3],self.obs_ph[4]:active_obs[4],self.obs_ph[5]:active_obs[5]})
             active_obs = active_obs[:self.num_agents]
