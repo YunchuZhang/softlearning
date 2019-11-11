ec2118c49daf5fdd75e5670f889f9dd0057ef612
Yash-5 <ychandar@cs.cmu.edu>
2019-11-10 21:34:39 -0500

    pretrain save

--- examples/map3D/pretrain_discovery.py
+++ examples/map3D/pretrain_discovery.py
@@ -1,8 +1,10 @@
 import os
 import numpy as np
+from collections import defaultdict
 
 import tensorflow as tf
 
+from softlearning.algorithms.utils import map3D_save, map3D_load
 from softlearning.environments.adapters.gym_adapter import GymAdapter
 from softlearning.replay_pools.simple_replay_pool import SimpleReplayPool
 from softlearning.policies.utils import get_policy
@@ -15,6 +17,7 @@ exploration_steps = 500
 train_iters = 10000
 sample_steps_per_iter = 2
 do_cropping = False
+map3D_scope = "memory"
 
 log_freq = hyp.log_freqs['train']
 
@@ -36,6 +39,8 @@ session = tf.keras.backend.get_session()
 
 env = GymAdapter('SawyerMulticameraPushRandomObjects',
                  'v0',
+                 num_cameras=2,
+                 track_object=True,
                  observation_keys=observation_keys)
 
 
@@ -50,7 +55,7 @@ sampler.initialize(env, policy, replay_pool)
 
 for i in range(exploration_steps):
     #print("sampling")
-    sampler.sample()
+    sampler.sample(do_cropping=do_cropping)
 
 checkpoint_dir = os.path.join("checkpoints", hyp.name)
 log_dir = os.path.join("logs_mujoco_online", hyp.name)
@@ -68,17 +73,27 @@ with tf.compat.v1.variable_scope("memory"):
 
 writer = model.all_writers['train']
 
+memory_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+
+key_vars = defaultdict(list)
+for x in memory_vars:
+    key_vars[x.name.split("/")[1]].append(x)
+
+map3D_saver = {}
+for k, v in key_vars.items():
+    map3D_saver[k] = tf.train.Saver(var_list=v, max_to_keep=None, restore_sequentially=True)
+
 for step in range(train_iters):
 
     #print("Step:", step)
 
     for sample_step in range(sample_steps_per_iter):
-        sampler.sample()
+        sampler.sample(do_cropping=do_cropping)
 
     log_this = np.mod(step, log_freq) == 0
     #log_this = False
 
-    #if log_this:
-    #    print("Should log!")
+    if log_this:
+        map3D_save(session, "/home/ychandar/pretrained_with_log", map3D_saver, step // log_freq)
 
     model.train_step(step, 'train', sampler.random_batch(), True, writer, log_this, do_cropping=do_cropping)
